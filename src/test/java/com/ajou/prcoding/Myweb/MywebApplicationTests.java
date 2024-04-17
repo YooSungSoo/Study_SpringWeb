14d3ecb1a68a59c5364fcf2d556951041e13515a
Yoo SungSoo <elcane2@ajou.ac.kr>
2024-04-17 16:47:57 +0900

    first commit

---
+++ src/test/java/com/ajou/prcoding/Myweb/MywebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajou.prcoding.Myweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MywebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
