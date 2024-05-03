ee146fb9f332fd9fdd7714cfb117e04e717e640a
Yoo SungSoo <elcane2@ajou.ac.kr>
2024-05-03 13:04:33 +0900

    musicSearch

--- src/main/java/com/ajou/prcoding/Myweb/controller/MyWebController.java
+++ src/main/java/com/ajou/prcoding/Myweb/controller/MyWebController.java
@@ -1,46 +1,48 @@
 package com.ajou.prcoding.Myweb.controller;
-import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import com.ajou.prcoding.Myweb.dto.*;
-import com.ajou.prcoding.Myweb.entity.FavoriteMusic;
-import com.ajou.prcoding.Myweb.service.MusicService;
 
-@RestController
+import java.io.IOException;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
+
+import com.ajou.prcoding.Myweb.dto.MusicList;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class MyWebController {
+    
+    RestTemplate restTemplate = new RestTemplate(); 
 
-    @Autowired
-    MusicService service;
+    @GetMapping(value="/musicSearch/{term}")
+    public MusicList musicSearchByPath(@PathVariable String term) {
 
-    @GetMapping(value = "/musicSearch/{name}")
-    public MusicList musicSearchByPath(@PathVariable String name){
-        return service.searchMusic(name);
-    }
-
-    @GetMapping(value="/musicSearch")
-    public MusicList musicSearchByParam(@RequestParam(value="term") String name) {
-        return service.searchMusic(name);
-    }
-    @GetMapping(value="/likes")  //Get Favorite Music list from Database
-    public List<FavoriteMusic> getLikes() {
-        return service.getLikes();
-    }
-
-    @PostMapping(value="/likes")
-    public int postLikes(@RequestBody FavoriteMusicRequestDto favorite) {
-        return service.saveFavorite(favorite);
-    }
-
-    @DeleteMapping(value="/likes/{id}")
-    public ResponseEntity<String> deleteFavorite(@PathVariable String id) {
         try {
-            service.deleteFavorite(id);
-            return ResponseEntity.ok("Favorite with ID " + id + " deleted successfully");
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                                 .body("Failed to delete favorite with ID " + id);
-        }
+            String response = restTemplate.getForObject("https://itunes.apple.com/search?term="+ term +"&entity=album ", String.class);
+            ObjectMapper mapper = new ObjectMapper();
+            MusicList list = mapper.readValue(response, MusicList.class);
+            System.out.println(list.getResultCount());
+            return list;
+          } catch(IOException e) {
+            System.out.println(e.toString());
+            return null;
+          }
+        
+     }
+    
+    @GetMapping(value="/musicSearch")
+    public MusicList musicSearchByParam(@RequestParam String term){
+
+        try {
+            String response = restTemplate.getForObject("https://itunes.apple.com/search?term="+term+"&entity=album ", String.class);
+            ObjectMapper mapper = new ObjectMapper();
+            MusicList list = mapper.readValue(response, MusicList.class);
+            System.out.println(list.getResultCount());
+            return list;
+          } catch(IOException e) {
+            System.out.println(e.toString());
+            return null;
+          }
+        
     }
 }
