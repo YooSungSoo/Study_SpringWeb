a526c26620d0dd45d33ae686b2d9050b510b01e4
Yoo SungSoo <elcane2@ajou.ac.kr>
2024-05-07 18:38:26 +0900

    post_likes

--- src/main/java/com/ajou/prcoding/Myweb/controller/MyWebController.java
+++ src/main/java/com/ajou/prcoding/Myweb/controller/MyWebController.java
@@ -4,11 +4,15 @@ import java.util.List;
 import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
 
+import com.ajou.prcoding.Myweb.dto.FavoriteMusicRequestDto;
 import com.ajou.prcoding.Myweb.dto.MusicList;
 import com.ajou.prcoding.Myweb.entity.FavoriteMusic;
 import com.ajou.prcoding.Myweb.repository.FavoriteRepository;
@@ -63,6 +67,20 @@ public class MyWebController {
             System.out.println(e.toString());
             return null;
         }
+
+        
 }
+@PostMapping(value="/likes")
+@Transactional
+public int postLikes(@RequestBody FavoriteMusicRequestDto favorite) {
+    FavoriteMusic music = albumsRepo.save(favorite.toEntity());
+    if(music != null) {
+        return 1;
+    }
+    else {
+        return 0;
+    }
+}
+
 
 }
